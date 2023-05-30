2cf2245e7c987eedb4b805195b2977ac6412fb19
maykongsn <contatomaykon@hotmail.com>
2023-05-30 09:40:14 -0300

    refactor: removing console message

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    console.log("banco iniciado")
     await this.$connect();
   }
 
