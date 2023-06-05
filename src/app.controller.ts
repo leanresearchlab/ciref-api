62a8d2509506d3df7df96a9a8263e86a547771a2
maykongsn <contatomaykon@hotmail.com>
2023-06-05 13:56:38 -0300

    fix: removing branch name

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
 
   @Post('refact')
   extractRefacts(@Body() body): Promise<any> {
-    return this.appService.extractRefacts(body.username, body.url, body.branch);
+    return this.appService.extractRefacts(body.username, body.url);
   }
 
   @Post('payload')
